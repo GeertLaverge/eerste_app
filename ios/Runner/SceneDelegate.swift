98abd8173cb82fadba63d73901283628252e6efd
Geert <info@thimaco.be>
2026-05-27 10:57:05 +0200

    Fix iOS auth redirect

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -2,5 +2,13 @@ import Flutter
 import UIKit
 
 class SceneDelegate: FlutterSceneDelegate {
-
+  override func scene(
+    _ scene: UIScene,
+    openURLContexts URLContexts: Set<UIOpenURLContext>
+  ) {
+    super.scene(
+      scene,
+      openURLContexts: URLContexts
+    )
+  }
 }
