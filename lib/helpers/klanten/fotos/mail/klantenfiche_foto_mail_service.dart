876d3311ce6e2a43b751e61ea52b9d28535cadfb
Geert <info@thimaco.be>
2026-06-06 17:06:50 +0200

    Fix RepaintBoundary import

--- lib/helpers/klanten/fotos/mail/klantenfiche_foto_mail_service.dart
+++ lib/helpers/klanten/fotos/mail/klantenfiche_foto_mail_service.dart
@@ -14,65 +14,25 @@ class KlantenficheFotoMailService {
   }) async {
     try {
       final token = await OneDriveAuthService().login();
-      final gebruikerResponse = await http.get(
-        Uri.parse('https://graph.microsoft.com/v1.0/me'),
-        headers: {
-          'Authorization': 'Bearer $token',
-        },
-      );
-
-      print('GEBRUIKER: ${gebruikerResponse.body}');
 
       if (token.startsWith('FOUT')) {
         return token;
       }
-      print('ONTVANGER: $ontvanger');
-      print('AANTAL FOTO\'S: ${fotos.length}');
 
-      final attachments = <Map<String, dynamic>>[];
-
-      for (final foto in fotos) {
-        final bytes = await foto.readAsBytes();
-
-        attachments.add({
-          '@odata.type': '#microsoft.graph.fileAttachment',
-          'name': foto.uri.pathSegments.last,
-          'contentType': 'image/jpeg',
-          'contentBytes': base64Encode(bytes),
-        });
-      }
-
-      final body = {
-        'message': {
-          'subject': onderwerp,
-          'body': {
-            'contentType': 'Text',
-            'content': bericht,
-          },
-          'toRecipients': [
-            {
-              'emailAddress': {
-                'address': ontvanger,
-              },
-            },
-          ],
-          'attachments': attachments,
-        },
-        'saveToSentItems': true,
-      };
-
-      final response = await http.post(
+      final gebruikerResponse = await http.get(
         Uri.parse(
-          'https://graph.microsoft.com/v1.0/me/sendMail',
+          'https://graph.microsoft.com/v1.0/me',
         ),
         headers: {
           'Authorization': 'Bearer $token',
-          'Content-Type': 'application/json',
         },
-        body: jsonEncode(body),
       );
 
-      return 'STATUS ${response.statusCode}\n${response.body}';
+      return '''
+STATUS: ${gebruikerResponse.statusCode}
+
+${gebruikerResponse.body}
+''';
     } catch (e) {
       return 'MAIL_EXCEPTION: $e';
     }
